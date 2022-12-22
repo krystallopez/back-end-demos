1be9a98500659afbbf0e9695ecdbf9c83341a5dc
davidalantodd <david.todd@multiverse.io>
2022-12-22 11:05:20 -0500

    add demo starter code week 1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # back-end-demos
+
+### to start:
+run 'npm run seed'
+
+run 'npm run start' OR 'npm run start-dev'
+
+test via Postman
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const { Dog } = require('./db');
+const { sequelize } = require('./db/db');
+const { Op } = require('sequelize');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.post('/dogs', async (req, res, next) => {
+  try {
+    const {name, breed, color, description} = req.body;
+    const dog = await Dog.create({name, breed, color, description});
+    res.send(dog);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete('/dogs/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params;
+    const existingDog = await Dog.findByPk(id);
+    if(!existingDog) {
+      res.status(404).send(`Dog with id ${id} not found`);
+      return;
+    }
+    await Dog.destroy({where: {id}});
+    res.send(`deleted dog with id ${id}`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/dogs', async (req, res, next) => {
+  try {
+    //TO DO
+    res.send("need to complete this GET route");
+  } catch (error) {
+    next(error)
+  }
+});
+
+const { PORT = 4000 } = process.env;
+
+app.listen(PORT, () => {
+  sequelize.sync({ force: false });
+  console.log(`Dogs are ready at http://localhost:${PORT}`);
+});
